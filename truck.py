9ca391f84238956bb99a1870b30084f6455b0d3d
Juanizaki <tony.ramos.dev@gmail.com>
2026-04-05 13:23:03 -0400

    refactored Truck class to simplify initialization and enhance time tracking

--- truck.py
+++ truck.py
@@ -1,76 +1,39 @@
+# student id: 011028882
+
 # class defining truck objects
+
+import datetime
 from package import Package
-from parse_csv import ParseCSV
 
 
 class Truck:
-    available_trucks: list[int] = [1, 2, 3]
-    available_drivers: list[str] = ["Juan", "Tony"]
-
     def __init__(
         self,
         truck: int,
         driver: str,
-        package_load: int,
-        package_list: list[Package],
-        current_location: str,
-        distance_traveled: int,
+        departure_time: datetime.datetime,
     ) -> None:
         self.truck: int = truck
         self.driver: str = driver
-        self.package_max_load: int = 16  # total number of packages a truck can carry
-        self.package_load: int = package_load  # number of packages on the truck
-        self.package_list: list[Package] = package_list
-        self.current_location: str = current_location
-        self.next_location = None
-        self.distance_traveled: int = distance_traveled
+        self.package_max_load: int = 16  # max 16 packages per truck
+        self.package_list: list[Package] = []
+        self.distance_traveled: float = 0.0
+        # constant 18 mph used to convert distance legs into time deltas
         self.speed: int = 18
+        self.departure_time: datetime.datetime = departure_time
+        # current_time starts at departure and advances with each leg during runtime
+        self.current_time: datetime.datetime = departure_time
+        # ordered list of (timestamp, cumulative_miles) entries
+        # used by mileage_at_time() to answer point-in-time mileage queries
+        self.mileage_log: list[tuple[datetime.datetime, float]] = []
 
     def __str__(self) -> str:
         return (
             f"{self.truck}\n"
             f"{self.driver}\n"
             f"{self.package_max_load}\n"
-            f"{self.package_load}\n"
             f"{self.package_list}\n"
-            f"{self.current_location}\n"
-            f"{self.next_location}\n"
             f"{self.distance_traveled}\n"
             f"{self.speed}\n"
+            f"{self.departure_time}\n"
         )
-
-    def __repr__(self) -> str:
-        return (
-            f"Truck: {self.truck!r}\n"
-            f"Driver: {self.driver!r}\n"
-            f"Package Max Load: {self.package_max_load!r}\n"
-            f"Package Load: {self.package_load!r}\n"
-            f"Package List: {self.package_list!r}\n"
-            f"Current Location: {self.current_location!r}\n"
-            f"Next Location: {self.next_location!r}\n"
-            f"Distance Traveled: {self.distance_traveled!r}\n"
-            f"Speed: {self.speed!r}\n"
-        )
-
-
-# testing
-# truck1 = Truck(
-#     truck=Truck.available_trucks[0],
-#     driver=Truck.available_drivers[0],
-#     package_load=10,
-#     package_list=ParseCSV.parse_csv("data/packages.csv"),
-#     current_location="Hub",
-#     distance_traveled=0,
-# )
-#
-# truck2 = Truck(
-#     truck=Truck.available_trucks[1],
-#     driver=Truck.available_drivers[1],
-#     package_load=8,
-#     package_list=[],
-#     current_location="Hub",
-#     distance_traveled=0,
-# )
-#
-# print(repr(truck1))
-# print(truck2)
